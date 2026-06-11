3765e5d7c1fc973567feb172dd8c0e097360093c
It'smail <ismailnuralam@gmail.com>
2026-06-11 12:28:10 +0700

    Change output astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    adapter: cloudflare(),
+  output: 'static',
+  adapter: cloudflare(),
 });
