403dab492956de259ebc02c220338226790c6ec9
It'smail <ismailnuralam@gmail.com>
2026-07-11 18:45:16 +0700

    [main] rewrite blog.itsmail.dev to /blog, absolute nav for other domains

---
+++ src/middleware.ts
@@ -0,0 +1,14 @@
+import { defineMiddleware } from "astro:middleware";
+
+const BLOG_HOST = "blog.itsmail.dev";
+
+export const onRequest = defineMiddleware((context, next) => {
+  const { url } = context;
+
+  if (url.hostname === BLOG_HOST && !url.pathname.startsWith("/blog")) {
+    url.pathname = `/blog${url.pathname === "/" ? "" : url.pathname}`;
+    return next(url);
+  }
+
+  return next();
+});
